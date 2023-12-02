51da9393783642a6c16c46e1bf01dd95d760b2f5
arhvnn <abdessamed.zetroni@ensia.edu.dz>
2023-12-02 09:51:03 +0100

    reloaded

---
+++ Headers/Country/Country.h
@@ -0,0 +1,29 @@
+#ifndef COUNTRYTREE_H
+#define COUNTRYTREE_H
+
+#include<iostream>
+using namespace std;
+
+struct Country{
+    string name;
+    Country* left;
+    Country* right;
+
+    Country(string n);
+    ~Country();
+};
+
+class CountryTree{
+    Country* root;
+    void Print(Country* root);
+public:
+    CountryTree();
+    ~CountryTree();
+    void InsertCountry(string country_name);
+    Country* SearchCountry(string country_name);
+    void DeleteCountry(string country_name);
+    void PrintCountries(Country* root);
+    void EditCountry(string country_name, string new_country_name);
+};
+
+#endif // COUNTRYTREE_H
